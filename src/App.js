542768dcafbef2527e6a8c9d83bdcd047ff3b6f7
Oluwatosin, Akinyele <oluwatosin.akinyele@andela.com>
2019-07-09 11:11:34 +0100

    feat(recipe): get single recipe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React,{Fragment} from 'react';
+import React, { useState, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './components/layout/Navbar';
 import Index from './components/layout/Index';
-import { Provider } from './context';
+import FullRecipe from './components/recipes/fullRecipe';
+import { RecipeContext } from './context';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [recipes, setRecipes] = useState({
+    recipes: [],
+    heading: 'Featured Recipes',
+  });
   return (
-    <Provider>
-    <Router>
-    <Fragment>
-      <NavBar/>
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Index}/>
-        </Switch>
-      </div>
-    </Fragment>
-    </Router>
-    </Provider>
+    <RecipeContext.Provider value={{ recipes, setRecipes }}>
+      <Router>
+        <Fragment>
+          <NavBar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+              <Route exact path="/recipe/:id" component={FullRecipe} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </RecipeContext.Provider>
   );
-}
+};
 
 export default App;
--- src/components/layout/Index.jsx
+++ src/components/layout/Index.jsx
@@ -1,11 +1,11 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import Recipes from '../recipes/Recipes';
 
 const Index = () => {
-    return (
-        <Fragment>
-         <Recipes/>
-        </Fragment>
-    )
-}
-export default  Index;
+  return (
+    <Fragment>
+      <Recipes />
+    </Fragment>
+  );
+};
+export default Index;
